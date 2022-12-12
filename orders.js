75db0ebeef633c98ba01c400c5648d9618dd9d85
OmarElDwary <105021882+OmarElDwary@users.noreply.github.com>
2022-12-13 00:14:00 +0200

    first commit

---
+++ orders.js
@@ -0,0 +1,38 @@
+const Orders = [
+    {
+        productNumber: '1',
+        productName: 'HikVision Camera 4MB',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productNumber: '2',
+        productName: 'HikVision DVR 4MB',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productNumber: '3',
+        productName: 'CAT6 Sweedy 300M',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productNumber: '4',
+        productName: 'TP Link Access point',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productNumber: '5',
+        productName: 'Redragon Mouse',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productNumber: '6',
+        productName: 'T-Dagger Keyboard',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    }
+]
---
+++ script.js
@@ -0,0 +1,23 @@
+const sideMenu = document.querySelector("aside");
+const menuBtn = document.querySelector("#menu-btn");
+const closeBtn = document.querySelector("#close-btn");
+const modeToggler = document.querySelector(".theme-toggler")
+
+modeToggler.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode-variable');
+    
+    modeToggler.querySelector('i:nth-child(1)').classList.toggle('active');
+    modeToggler.querySelector('i:nth-child(2)').classList.toggle('active');
+})
+Orders.forEach(order => {
+    const tr = document.createElement('tr');
+    const trContent = `
+                        <td>${order.productNumber}</td>
+                        <td>${order.productName}</td>
+                        <td>${order.paymentStatus}</td>
+                        <td class=" ${order.shipping === 'Declined' ? 'danger' : order.shipping === 'pending' ? 'warning' : order.shipping === 'Delivered' ? 'success' : 'primary' }"> ${order.shipping} </td>
+                        <td class="primary">Details</td>
+                        `;
+    tr.innerHTML = trContent;
+    document.querySelector('table tbody').appendChild(tr);
+})
